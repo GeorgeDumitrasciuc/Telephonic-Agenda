357a8e471ea76d1841b9be1f4750c13bc01b02ef
George Dumitrasciuc <106991594+GeorgeDumitrasciuc@users.noreply.github.com>
2023-09-05 14:48:42 +0300

    Create agenda.cpp

---
+++ agenda.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Contact {
+    string nume;
+    string numar_telefon;
+};
+
+void adaugaContact(vector<Contact>& agenda) {
+    Contact contact;
+    cout << "Introduceti numele contactului: ";
+    cin >> contact.nume;
+    cout << "Introduceti numarul de telefon: ";
+    cin >> contact.numar_telefon;
+    agenda.push_back(contact);
+    cout << "Contact adaugat cu succes!" << endl;
+}
+
+void afiseazaAgenda(const vector<Contact>& agenda) {
+    cout << "Agenda telefonica:" << endl;
+    for (const Contact& contact : agenda) {
+        cout << "Nume: " << contact.nume << ", Numar de telefon: " << contact.numar_telefon << endl;
+    }
+}
+
+void salveazaInFisier(const vector<Contact>& agenda) {
+    ofstream fout("date.txt");
+    if (fout.is_open()) {
+        for (const Contact& contact : agenda) {
+            fout << contact.nume << " " << contact.numar_telefon << endl;
+        }
+        fout.close();
+        cout << "Contactele au fost salvate in fisierul date.txt!" << endl;
+    }
+    else {
+        cout << "Eroare la deschiderea fisierului!" << endl;
+    }
+}
+
+int main() {
+    vector<Contact> agenda;
+    ifstream fin("date.txt");
+    if (fin.is_open()) {
+        string nume, numar_telefon;
+        while (fin >> nume >> numar_telefon) {
+            Contact contact;
+            contact.nume = nume;
+            contact.numar_telefon = numar_telefon;
+            agenda.push_back(contact);
+        }
+        fin.close();
+    }
+    else {
+        cout << "Nu s-a putut deschide fisierul date.txt. Un fisier nou va fi creat." << endl;
+    }
+
+    while (true) {
+        cout << "1. Adauga contact" << endl;
+        cout << "2. Afiseaza agenda" << endl;
+        cout << "3. Salveaza in fisier" << endl;
+        cout << "4. Iesire" << endl;
+        int optiune;
+        cin >> optiune;
+
+        switch (optiune) {
+        case 1:
+            adaugaContact(agenda);
+            break;
+        case 2:
+            afiseazaAgenda(agenda);
+            break;
+        case 3:
+            salveazaInFisier(agenda);
+            break;
+        case 4:
+            return 0;
+        default:
+            cout << "Optiune invalida. Va rugam sa introduceti o optiune valida." << endl;
+        }
+    }
+
+    return 0;
+}
